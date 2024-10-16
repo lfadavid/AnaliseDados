46b0c7ead55f44c6f20753d62ef6fa47d603d5e7
lfadavid <167301084+lfadavid@users.noreply.github.com>
2024-10-16 07:29:43 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -27,12 +27,13 @@ if uploader_file is not None:
     
     st.success("Conseguimos Carregar os seus dados :)")
     
-    def formatar(valor):
-        return "{:,.2f}".format(valor)
-    
+    def corrigir_nomes(nome):
+        nome = nome.replace('ARC-', '').replace('MILK RUN', '').replace('ARC-SZ01WR - WESTROCK', 'WESTROCK').replace('COT052 - AEROPORTO - SZ X VCP', 'Aeroporto VCP')
+        return nome
     
     with st.sidebar:
         estado_destino = df["Estado Destino"].unique().tolist()
+        df['Tabela de Preço'] = df["Tabela de Preço"].apply(corrigir_nomes)
         df["Coleta"] = pd.to_datetime(df["Coleta"])
         df["Mes"] = df["Coleta"].dt.month_name(locale.setlocale(locale.LC_ALL,'pt_BR.UTF-8'))
         
@@ -52,33 +53,56 @@ if uploader_file is not None:
                         default=df["Estado Destino"].unique()
                         )
 
-    df = df.query("Mes == @mes and `Estado Destino` == @estado_destino")
+    tabela_preco = st.sidebar.multiselect(
+                        key=3,
+                        label="Tabela de Preço",
+                        options=df["Tabela de Preço"].unique(),
+                        default=None
+                        )
+                    
+    df = df.query("Mes == @mes and `Estado Destino` == @estado_destino and `Tabela de Preço` == @tabela_preco")
 
  
     st.header("Análise de Dados retirados do sistema.", divider='gray')
     
-    total_peso = f"{round(df["Peso Real"].sum(),2):,.0f}"
-    total_nf = f"{len(df['Nota Fiscal'].value_counts()):,.0f}"
-    valor_tranportado = f"{round(df['Valor N.F.'].sum(),2):,.0f}"
-    total_frete_peso =f" R$ {round(df['Frete Peso'].sum(),2):,.0f}"
+    # Criação de Cards
     
-    col1,col2,col3, col4 = st.columns(4)
-                #col1.metric("Total de Peso", total_peso)
-    col1.metric(label="Total de Peso Carregado", value=total_peso)
-    col2.metric("Total de Nota Fiscal", value=total_nf)
-    col3.metric("Total de Frete Peso", total_frete_peso)
-    col4.metric("Total de valor transportado", valor_tranportado)
+    def criar_card(icone, numero, texto , coluna_card):
+        container = coluna_card.container(border=True)
+        coluna_esquerda , coluna_direita = container.columns([1, 2.5])
+        coluna_esquerda.image(f"imagens/{icone}")
+        coluna_direita.write(numero)
+        coluna_direita.write(texto)
+        
+    coluna_esquerda , coluna_meio , coluna_direita = st.columns([1, 1, 1])
     
-    colunas_aparecer = ["Número CT-e","Destinatário","Cidade Destinatário", "Tabela de Preço", "Nota Fiscal","Peso Real","Usuário cadastro", "Coleta"]
-    st.dataframe(df[colunas_aparecer],use_container_width=True,
+    criar_card("Peso.png",f'{df["Peso Real"].sum():,.2f}',"Total de Peso (Toneladas)", coluna_esquerda)
+    criar_card("notafiscal.png",f'{df["Nota Fiscal"].count():,}', "Total de Nota Fiscal (Und)", coluna_meio)
+    criar_card("fretepeso.png",f' R$ {df["Frete Peso"].sum():,.2f}', "Total de Frete Peso (R$)", coluna_direita)
+    
+    criar_card("valortransportado.png",f'R$ {df["Valor N.F."].sum():,.2f}', "Valor Transportado (R$)", coluna_esquerda)
+    criar_card("icms.png",f'R$ {df["Valor ICMS"].sum():,.2f}', "Valor Gasto com ICMS (R$)", coluna_meio)
+    criar_card("seguro.png",f'R$ {df["ADValorem"].sum():,.2f}', "Valor do ADValorem (R$)", coluna_direita)
+
+#Area do GRafico
+
+
+base_mensal = df.groupby(df["Coleta"].dt.to_period("M")).sum(numeric_only=True).reset_index()
+base_mensal["Coleta"] = base_mensal["Coleta"].dt.to_timestamp()
+
+container = st.container(border=False)
+with container:
+    
+        #grafico de area
+        st.write("### Total de Frete Peso por Mês (R$)")
+        grafico_area = px.area(base_mensal, x="Coleta", y="Frete Peso")
+        st.plotly_chart(grafico_area)
+           
+colunas_aparecer = ["Número CT-e","Destinatário","Cidade Destinatário", "Tabela de Preço", "Nota Fiscal","Peso Real","Usuário cadastro", "Coleta"]
+st.dataframe(df[colunas_aparecer],use_container_width=True,
                   column_config= { "Coleta": st.column_config.DateColumn("Data Faturamento",  format="DD.MM.YYYY",
             step=1,)
-                      },hide_index=True)
-    
-    fig = px.bar(df, x=df["Mes"], y=df["Frete Peso"], title="Faturamento Mensal",color=df["Estado Destino"])
-    
-    st.plotly_chart(fig)
-
-    st.write("""
+                      },hide_index=True)        
+st.write("""
          &copy; 2024 - Luis Felipe A. David. Todos os direitos reservados
          """)
